660824c9cefaa42aba0e8ebf3d89fc3d5250c3f2
KrazyManJ <KrazyManJ@gmail.com>
2022-04-13 19:30:03 +0200

    Added forgotten CooldownTimer Class

---
+++ src/main/java/me/KrazyManJ/KrazyEngine/Any/CooldownTimer.java
@@ -0,0 +1,47 @@
+package me.KrazyManJ.KrazyEngine.Any;
+
+import java.util.concurrent.TimeUnit;
+
+@SuppressWarnings({"unused","UnusedReturnValue"})
+public final class CooldownTimer {
+    private long delay;
+    private long tick = System.currentTimeMillis();
+
+    public CooldownTimer(long delay){
+        this.delay = delay;
+    }
+    public CooldownTimer(long delay, TimeUnit unit){
+        this.delay = unit.toMillis(delay);
+    }
+
+    public long getDelay() {
+        return delay;
+    }
+    public CooldownTimer setDelay(long delay) {
+        this.delay = delay;
+        return this;
+    }
+    public CooldownTimer setDelay(long delay, TimeUnit unit){
+        this.delay = unit.toMillis(delay);
+        return this;
+    }
+
+    public boolean check(){
+        return System.currentTimeMillis() > tick + delay;
+    }
+    public CooldownTimer tick(){
+        tick = System.currentTimeMillis();
+        return this;
+    }
+    public boolean checkAndTick(){
+        if (check()){
+            tick();
+            return true;
+        }
+        return false;
+    }
+    public CooldownTimer clear(){
+        tick = -delay;
+        return this;
+    }
+}
