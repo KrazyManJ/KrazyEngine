46dd31ba60f1517f3d369cd7411623f4f1b9aed7
KrazyManJ <KrazyManJ@gmail.com>
2022-04-21 21:57:00 +0200

    Converted RegexGroupMatch class from record to classic class due to compatibility with java versions

--- src/main/java/me/KrazyManJ/KrazyEngine/Any/Regex/RegexGroupMatch.java
+++ src/main/java/me/KrazyManJ/KrazyEngine/Any/Regex/RegexGroupMatch.java
@@ -1,5 +1,32 @@
 package me.KrazyManJ.KrazyEngine.Any.Regex;
 
 import org.intellij.lang.annotations.Language;
+import org.jetbrains.annotations.NotNull;
 
-public record RegexGroupMatch(String group, @Language("RegExp") String value) {}
+import java.util.Objects;
+
+@SuppressWarnings("unused")
+public final class RegexGroupMatch {
+    private final String group;
+    private final String value;
+
+    public RegexGroupMatch(@NotNull @Language("SQL") String group,@NotNull String value) {
+        this.group = group;
+        this.value = value;
+    }
+    public String getGroup() { return group; }
+    public String getValue() { return value; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RegexGroupMatch that = (RegexGroupMatch) o;
+        return group.equals(that.group) && value.equals(that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(group, value);
+    }
+}
