fa878287dfbd6cff2172aec37f4bd27452582081
KrazyManJ <KrazyManJ@gmail.com>
2022-04-21 21:47:25 +0200

    Cleared SpigotMain class for test code, added shade plugin for compiling with dependencies

--- src/main/java/me/KrazyManJ/KrazyEngine/SpigotMain.java
+++ src/main/java/me/KrazyManJ/KrazyEngine/SpigotMain.java
@@ -2,10 +2,15 @@ package me.KrazyManJ.KrazyEngine;
 
 
 import me.KrazyManJ.KrazyEngine.Any.Command.TabCompleteUtils;
+import me.KrazyManJ.KrazyEngine.Any.Text.RomanNumber;
 import me.KrazyManJ.KrazyEngine.Spigot.CommandMapRegistry;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.FurnaceRecipe;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
@@ -19,15 +24,18 @@ public final class SpigotMain extends JavaPlugin {
         super.onEnable();
         CommandMapRegistry.register(new Command("testengine") {
             @Override
-            public boolean execute(@NotNull CommandSender commandSender, @NotNull String s, @NotNull String[] strings) {
-                if (!(commandSender instanceof Player p)) return false;
-                //Test code here:
-                return true;
+            public boolean execute(@NotNull CommandSender sender, @NotNull String label, @NotNull String[] args) {
+                if (!(sender instanceof Player p)) return false;
+                FurnaceRecipe r = new FurnaceRecipe(NamespacedKey.minecraft("test"), new ItemStack(Material.STONE),Material.STONE,1,1);
 
+
+
+
+                return true;
             }
             @NotNull
             @Override
-            public List<String> tabComplete(@NotNull CommandSender sender, @NotNull String alias, @NotNull String[] args) throws IllegalArgumentException {
+            public List<String> tabComplete(@NotNull CommandSender sender, @NotNull String label, @NotNull String[] args) throws IllegalArgumentException {
                 return TabCompleteUtils.clearTabComplete();
             }
         });
